81fbcda96634c8f742f2f28d604aa6713af03f6c
Magno lemos <magnolemos@Magnos-MacBook-Pro.local>
2019-11-19 21:52:34 -0300

    First commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+  ]
+})
+export class SharedModule { }
